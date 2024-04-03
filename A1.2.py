c7a51447e8a2179adb6952c59314c2c8a710f37f
indira maurya <maurya.10@iitj.ac.in>
2024-04-04 02:57:17 +0530

    A1.2 file

---
+++ A1.2.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Sigmoid activation function
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+# Input data
+random_values = [-3.5, -1.2, 0, 2.8, -4.1, 1.5, -0.7, 3.2, -2.4, 4.6]
+
+# Generate sigmoid graph for the input data
+x = np.array(random_values)
+y = sigmoid(x)
+
+# Plot
+plt.plot(x, y, marker='o', linestyle='-')
+plt.title('Sigmoid Activation Function')
+plt.xlabel('Input')
+plt.ylabel('Output')
+plt.grid(True)
+plt.show()
